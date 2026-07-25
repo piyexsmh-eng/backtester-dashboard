b902296662a18e8b7772978e3d121a9230242e6e
piyexsmh <piuexsmh@gmail.com>
2026-07-25 22:31:20 +0000

    Add global error handler to debug white screen issue

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+window.addEventListener('error', (event) => {
+  alert('ERROR: ' + event.message + '\nFile: ' + event.filename + '\nLine: ' + event.lineno);
+});
+
+window.addEventListener('unhandledrejection', (event) => {
+  alert('PROMISE ERROR: ' + (event.reason?.message || event.reason));
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
